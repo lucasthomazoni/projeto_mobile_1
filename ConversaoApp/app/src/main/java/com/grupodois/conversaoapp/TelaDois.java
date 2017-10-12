9b40b68e06594d2455369deff1a3b8f42d8f8f8a
Juan Felipe <juanfkrassmann@gmail.com>
2017-10-12 16:46:12 -0300

    Alterações dos elementos da tela 2

--- ConversaoApp/app/src/main/java/com/grupodois/conversaoapp/TelaDois.java
+++ ConversaoApp/app/src/main/java/com/grupodois/conversaoapp/TelaDois.java
@@ -2,12 +2,40 @@ package com.grupodois.conversaoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class TelaDois extends AppCompatActivity {
+
+    private String[] moedas = new String[]{"Dólar", "Euro", "Pesos Chilenos", "Iene"};
+
+    private Spinner spinnerMoedas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_dois);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, moedas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinnerMoedas = (Spinner) findViewById(R.id.spinnerMoedas);
+        spinnerMoedas.setAdapter(adapter);
+
+        spinnerMoedas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 }
